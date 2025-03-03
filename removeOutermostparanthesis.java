659f6c76a07a4f4b15821caac3722d2df90b70bd
Beekay-exe <kumarvelpandian1966@gmail.com>
2025-03-03 13:14:56 +0530

    1021

---
+++ removeOutermostparanthesis.java
@@ -0,0 +1,25 @@
+public class removeOutermostparanthesis {
+    class Solution {
+        public String removeOuterParentheses(String s) {
+            StringBuilder result = new StringBuilder();
+            int balance = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
+                if (c == '(') {
+                    if (balance > 0) {
+                        result.append(c);
+                    }
+                    balance++;
+                } else {
+                    balance--;
+                    if (balance > 0) {
+                        result.append(c);
+                    }
+                }
+            }
+            return result.toString();
+        }
+    }
+
+}
